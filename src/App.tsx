14c7635acb56094bcdfa9697f1b72abfecdeae32
winsonyin <winsonyin123@gmail.com>
2020-08-30 18:00:40 +0800

    start qiankun main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { refreshToken } from "@mfe-common/auth"
+import axiosRetry from "@mfe-common/http"
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    // refreshToken("refreshToken")
+    // axiosRetry(axios, { retries: 3 })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+      <div id="frame"/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,68 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+    registerMicroApps,
+    addGlobalUncaughtErrorHandler,
+    setDefaultMountApp,
+    initGlobalState,
+    runAfterFirstMounted,
+    start,
+} from "qiankun";
+
+/**
+ * Step2 注册子应用
+ */
+
+registerMicroApps(
+    [
+        {
+            name: 'react16',
+            entry: '//localhost:7100',
+            container: '#frame',
+            activeRule: '/react16',
+        }
+    ],
+    {
+        beforeLoad: (app: any) => {
+            console.log("before load", app.name);
+            return Promise.resolve();
+        },
+    },
+);
+
+const {onGlobalStateChange, setGlobalState} = initGlobalState({
+    user: 'qiankun',
+});
+
+onGlobalStateChange((value, prev) => console.log('[onGlobalStateChange - master]:', value, prev));
+
+setGlobalState({
+    ignore: 'master',
+    user: {
+        name: 'master',
+    },
+});
+
+/**
+ * Step3 设置默认进入的子应用
+ */
+// setDefaultMountApp('/react16');
+
+/**
+ * Step4 启动应用
+ */
+start();
+
+runAfterFirstMounted(() => {
+    console.log('[MainApp] first app mounted');
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
